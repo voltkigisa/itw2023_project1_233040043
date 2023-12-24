93fd19446e96f4391cf765389f56c8f20eeab9db
voltkigisa <radenprawirajaya@gmail.com>
2023-12-24 11:58:02 +0700

    selesai

--- js/script.js
+++ js/script.js
@@ -4,11 +4,3 @@ const nav = document.querySelector ('nav ul');
 menuToggle.addEventListener ('click', function() {
     nav.classList.toggle('slide');
 })
-
-var typed= new Typed(".typing", {
-    Strings:[">>> Mahasiswa Universitas Pasundan"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop:true,
-})
