82153216438b436b792c39737be0eaf8538e1abc
voltkigisa <radenprawirajaya@gmail.com>
2023-12-24 10:07:22 +0700

    contact

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const nav = document.querySelector ('nav ul');
 menuToggle.addEventListener ('click', function() {
     nav.classList.toggle('slide');
 })
+
+var typed= new Typed(".typing", {
+    Strings:[">>> Mahasiswa Universitas Pasundan"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true,
+})
